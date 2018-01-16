7954084d867190fb5ccdcdf3d196bfb236285bb6
NRothera <31472463+NRothera@users.noreply.github.com>
2018-01-16 13:43:27 +0000

    cucumber connected

---
+++ spec/ui_tests/features/lib/codebar.rb
@@ -0,0 +1,9 @@
+require_relative 'pages/homepage'
+
+module CodebarSite
+
+  def homepage_page
+    HomePage.new
+  end
+
+end
---
+++ spec/ui_tests/features/step_definitions/open_homepage_defs.rb
@@ -0,0 +1,7 @@
+Given("I open the homepage") do
+  homepage_page.visit_homepage
+end
+
+Then("I should be on the homepage") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
