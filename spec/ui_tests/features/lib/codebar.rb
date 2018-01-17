90f6a848f35b8aa16fd656d2153d463bc5752311
Tech-A10 <tech-a10@Tech-A10s-MacBook-Air.local>
2018-01-17 11:41:40 +0000

    sign in test
    signing in with account tested

--- spec/ui_tests/features/lib/codebar.rb
+++ spec/ui_tests/features/lib/codebar.rb
@@ -1,9 +1,19 @@
 require_relative 'pages/homepage'
+require_relative 'pages/signing_in'
+require_relative 'pages/dashboard'
 
 module CodebarSite
 
   def homepage_page
     HomePage.new
+  end
+
+  def sign_in_page
+    SignIn.new
+  end
+
+  def dashboard
+    Dashboard.new
   end
 
 end
