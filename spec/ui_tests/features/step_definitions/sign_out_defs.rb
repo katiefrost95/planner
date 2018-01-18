d020a4fa49e7fff8d37c78792a418a913adf0176
NRothera <31472463+NRothera@users.noreply.github.com>
2018-01-18 15:22:22 +0000

    merged

--- spec/ui_tests/features/step_definitions/sign_out_defs.rb
+++ spec/ui_tests/features/step_definitions/sign_out_defs.rb
@@ -1,6 +1,6 @@
 Given("I am signed in") do
   homepage_page.visit_homepage
-  homepage_page.click_sign_in_link
+  nav_bar.click_sign_in_link
   sign_in_page.github_username("fake_github123@hotmail.com")
   sign_in_page.github_password("Thisisfake123")
   sign_in_page.click_sign_in_button
