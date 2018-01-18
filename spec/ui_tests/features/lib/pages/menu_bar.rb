eeb6b65377b47ee7454985d6b3804739e3031179
Tech-A08 <draymond@spartglobal.com>
2018-01-18 13:37:30 +0000

    added step defs to events feature and method to menu page

--- spec/ui_tests/features/lib/pages/menu_bar.rb
+++ spec/ui_tests/features/lib/pages/menu_bar.rb
@@ -7,5 +7,9 @@ class MenuBar
     find_link("My Dashboard").click
   end
 
+  def menu_subscriptions
+    find_link("Manage subscriptions").click
+  end
+
 
 end
--- spec/ui_tests/features/step_definitions/events_defs.rb
+++ spec/ui_tests/features/step_definitions/events_defs.rb
@@ -1,9 +1,14 @@
-Given("I am on the codebar homepage") do
-  home_page.visit_homepage
-end
+# Given("I am on the codebar homepage") do
+#   home_page.visit_homepage
+# end
 
 Given("I am subscribed as a coach to one of the locations") do
-  pending # Write code here that turns the phrase above into concrete actions
+  homepage_page.visit_homepage
+  homepage_page.click_sign_in_link
+  sign_in_page.github_username("fake_github123@hotmail.com")
+  sign_in_page.github_password("Thisisfake123")
+  sign_in_page.click_sign_in_button
+  menu_bar.menu_subscriptions
 end
 
 When("I click on events link") do
