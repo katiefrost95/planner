b53d56ea76367e46a99b0e687fbcbcec7a86c966
Tech-A10 <tech-a10@Tech-A10s-MacBook-Air.local>
2018-01-19 15:47:00 +0000

    removed after hook for sign out

--- spec/ui_tests/features/support/hooks.rb
+++ spec/ui_tests/features/support/hooks.rb
@@ -14,7 +14,7 @@ Before('@subscribe') do
   end
 end
 
-After('~@tutorials') do
+After('~@tutorials','~@sign_out') do
   nav_bar.click_menu
   homepage_page.click_log_out
   github_logout.visit_github
