9e1d7ec580d90f580198c0f1df88b6a8634a31b0
Tech-A10 <tech-a10@Tech-A10s-MacBook-Air.local>
2018-01-22 16:46:52 +0000

    fixing tests working together

--- spec/ui_tests/features/step_definitions/homepage_signup_defs.rb
+++ spec/ui_tests/features/step_definitions/homepage_signup_defs.rb
@@ -14,6 +14,10 @@ And("I authorize codebar to access my github") do
   sign_in_page.github_username("fake_github123@hotmail.com")
   sign_in_page.github_password("Thisisfake123")
   sign_in_page.click_sign_in_button
+  sleep 1
+  if current_url != "http://localhost:3000/dashboard"
+    sign_in_page.click_authorize_button
+  end
 end
 
 Then("I should be redirected to my dashboard") do
